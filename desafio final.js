cab37bfe8d75322bb3f9f0fea21edd1ee9909c89
leogomes2004 <68656742+leogomes2004@users.noreply.github.com>
2020-09-11 14:26:18 -0300

    desafio estacoes do ano
    desafio estacoes do ano

---
+++ desafio final.js
@@ -0,0 +1,21 @@
+<h1>Faça um script Javascript , com 12 botões numerados de 1 a 12, ao clicar em um dos botões deve escrever na
+     tela o nome do mês dentro de um título h2, escrever na tela qual é a estação do ano, mudar a cor de fundo
+      da tela de acordo com as 4 estações (verão: amarelo, outono: marrom, inverno: cinza, primavera: azul).
+       Na página deve ter uma imagem, que também deverá ser alterada conforme a estação do ano.</h1>
+
+       <imput>
+    <h1>   button
+           button
+           button
+           button
+           button
+           button
+           button
+           button
+           button
+           button
+           button
+           button
+           </h1>
+
+       </imput>
